d8a4577c53ca74caf34f77b634b77dc073fbba53
anas12312 <anashesham000@gmail.com>
2023-10-15 18:47:34 +0300

    search-bar

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {  Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Offers from './Pages/Offers';
 import Restaurants from './Pages/Restaurants';
-import Offer from './Pages/offer';
+import Offer from './Pages/Offer';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,7 +13,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{
+        headerShown: false
+      }}>
         <Stack.Screen name="Offers" component={Offers} />
         <Stack.Screen name="resturant" component={Restaurants}/>
         <Stack.Screen name="Offer" component={Offer}/>
--- Pages/offers.js
+++ Pages/offers.js
@@ -1,9 +1,12 @@
 import { View, Text, FlatList, Pressable, Image } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import config from '../config';
-
+import { TextInput } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 export default function Offers({navigation, route}) {
     const [offers, setOffers] = useState([]);
+    const [search, setSearch] = useState("");
     const [isLoading, setIsLoading] = useState(true)
     const offerPressed = (offerId, offerName,offerDescription, image_url) => {
         navigation.navigate("Offer", {
@@ -12,6 +15,12 @@ export default function Offers({navigation, route}) {
             description: offerDescription,
             img: image_url
         })
+    }
+    const doneSearch = () => {
+        
+    }
+    const searchHandler = () => {
+
     }
     useEffect(()=>{
         setIsLoading(true)
@@ -33,7 +42,21 @@ export default function Offers({navigation, route}) {
         })
     },[])
   return (
-    <View className="flex h-full w-full justify-center items-center bg-slate-500 flex-wrap p-1">
+    <View className="flex h-full w-full justify-center items-center bg-white flex-wrap mt-7">
+        <View className="flex justify-center items-center flex-row w-[95%] h-10 pl-2 mt-2 bg-gray-200 rounded-md">
+            <View className="text-red-200">
+                <FontAwesomeIcon icon={faSearch} size={20}/>
+            </View>
+            <TextInput
+                className="ml-2 flex justify-center items-center w-[90%] h-[90%]rounded-md"
+                placeholder='Search Offers, Restaurants, Tags...'
+            />
+        </View>
+        <View className="flex w-full h-12 bg-white rounded-md mt-1 flex-row justify-around items-center">
+            <View className="w-[30%] bg-slate-500 h-5/6 rounded-md flex justify-center items-center"><Text>Offers</Text></View>
+            <View className="w-[30%] bg-slate-500 h-5/6 rounded-md flex justify-center items-center"><Text>Restaurants</Text></View>
+            <View className="w-[30%] bg-slate-500 h-5/6 rounded-md flex justify-center items-center"><Text></Text></View>
+        </View>
         <FlatList
             data={offers}
             renderItem = {(offer) => {
@@ -43,8 +66,8 @@ export default function Offers({navigation, route}) {
                             <Image className="rounded-xl" source={{uri:offer.item.image_url, width:150,height:150 }}/>
                         </View>
                         <View className="p-1 pt-3">
-                            <Text className="text-white text-xl w-1/3 font-bold leading-6">{offer.item.name}</Text>  
-                            <Text className="text-white text-xs w-1/3  leading-5">{offer.item.description}</Text>   
+                            <Text className="text-xl w-1/3 font-bold leading-6">{offer.item.name}</Text>  
+                            <Text className="text-xs w-1/3  leading-5">{offer.item.description}</Text>   
                         </View>
                     </Pressable>
                 );
